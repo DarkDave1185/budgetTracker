850140f3552ac6c6e67f6329873d732eaefd0dba
DarkDave <darkdave1185@gmail.com>
2020-10-20 14:45:20 -0700

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/ClusterTest", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
